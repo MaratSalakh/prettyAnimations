a00b6c14de5813bd19416a2445e74f679bb5dbd4
Marr <marat.salah1313@gmail.com>
2023-11-18 18:08:13 +0100

    add CompressionPlugin HotModuleReplacementPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path');
+const CompressionPlugin = require("compression-webpack-plugin");
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new CompressionPlugin(), new webpack.HotModuleReplacementPlugin({})],
 }
